54dda837d4fdf84070fd13f26538bf678c576527
T3rabyte <43441096+T3rabyte@users.noreply.github.com>
2022-06-03 15:22:23 +0200

    Added a DataManager
    The DataManger reads the data file, then splits it up into the players and ball. To then send it to the corresponding objects.

---
+++ BeyondSportsApplicantTest/Assets/Scripts/Models/BallData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallData
+{
+    public int XPosition { get; set; }
+    public int YPosition { get; set; }
+    public int ZPosition { get; set; }
+    public int BallSpeed { get; set; }
+}
---
+++ BeyondSportsApplicantTest/Assets/Scripts/Models/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+    public int Team { get; set; }
+    public int TrackingID { get; set; }
+    public int PlayerNumber { get; set; }
+    public int XPosition { get; set; }
+    public int YPosition { get; set; }
+    public int PlayerSpeed { get; set; }
+}
